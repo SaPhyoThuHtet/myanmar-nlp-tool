b9636c58ca80c03408132e72423e8dc0f6ed8aa0
Sa Phyo Thu Htet <71957964+SaPhyoThuHtet@users.noreply.github.com>
2021-07-22 18:45:43 +0630

    Update nlp-tools.py

--- nlp-tools.py
+++ nlp-tools.py
@@ -7,7 +7,7 @@ st.sidebar.image(
     "images/peacock-3.png",
     width=200,
 )
-st.sidebar.markdown("<h3 style='text-align: center;'>NLP Tools</h1>", unsafe_allow_html=True)
+st.sidebar.markdown("<h3 style='text-align: center;'>NLP Tools by Sa Phyo Thu Htet</h1>", unsafe_allow_html=True)
 
 select = pd.DataFrame()
 select['topics'] = ['chracter-tokenization', 'syllable-tokenization', 'detect-email']
