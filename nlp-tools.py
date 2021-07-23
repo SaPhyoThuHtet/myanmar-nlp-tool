e0656495925aca561423a440c7dcf5820de07b21
Sa Phyo Thu Htet <71957964+SaPhyoThuHtet@users.noreply.github.com>
2021-07-23 08:00:22 +0630

    Update nlp-tools.py

--- nlp-tools.py
+++ nlp-tools.py
@@ -32,7 +32,7 @@ if(option == "syllable-tokenization"):
 if (option == 'syllbreak-zawgyi'):
     st.write("Description: Syllable break for Burmese in Zawgyi Encoding")
     user_input = st.text_input("Input", "သီဟိုဠ္မွ ဉာဏ္ႀကီးရွင္သည္ အာယုဝၯနေဆးၫႊန္းစာကို ဇလြန္ေဈးေဘး ဗာဒံပင္ထက္ အဓိ႒ာန္လ်က္ ဂဃနဏဖတ္ခဲ့သည္။ေဆာင္")
-    result = re.sub(r'(ေ*ျ*ႀ*ၿ*ၾ*[က-အ|႐|ႏ|ဥ|ဦ|႒]([က-အ]့*္[့း]*|[ါ-ာ]|[ိ-ူ]|[ဲ-္]|်|[ြ-ှ]|[ၐ-ၽ]|[ႁ-ႎ]|[႑-႟]){0,}|.)',r'\1 ',i.strip())
+    result = re.sub(r'(ေ*ျ*ႀ*ၿ*ၾ*[က-အ|႐|ႏ|ဥ|ဦ|႒]([က-အ]့*္[့း]*|[ါ-ာ]|[ိ-ူ]|[ဲ-္]|်|[ြ-ှ]|[ၐ-ၽ]|[ႁ-ႎ]|[႑-႟]){0,}|.)',r'\1 ',user_input.strip())
     st.write("Output:",result)
     
 if(option == "detect-email"):
