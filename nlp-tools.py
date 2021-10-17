4c17be3de453b1f85dfa0e2378a48224aaa8d1ba
Sa Phyo Thu Htet <71957964+SaPhyoThuHtet@users.noreply.github.com>
2021-10-18 03:09:39 +0630

    Update nlp-tools.py

--- nlp-tools.py
+++ nlp-tools.py
@@ -17,10 +17,7 @@ option = st.sidebar.selectbox(
 def file_selector(folder_path='.'):
     filenames = os.listdir(folder_path)
     selected_filename = st.selectbox('Select a file', filenames)
-    return os.path.join(folder_path, selected_filename)
-
-
-
+    return os.path.join(selected_filename)
 
 if(option == "time-domain"):
     filename = file_selector()
