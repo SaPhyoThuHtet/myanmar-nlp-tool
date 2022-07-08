b279fdacadde6f9142c03157d00b5026f2d8f69f
Sa Phyo Thu Htet <phyothuhtet.ai@gmail.com>
2022-07-08 13:47:12 +0630

    Update 1. n-gram with sliding windows approach.py

--- notebooks/1. n-gram with sliding windows approach.py
+++ notebooks/1. n-gram with sliding windows approach.py
@@ -1,3 +1,5 @@
+# Please refers to this notebook for time complexity analysis
+https://github.com/SaPhyoThuHtet/data-structure-and-algos/blob/main/useful
 a = ['▁', 'al', 'p', '▁ဗိုလ်', 'ချူပ်', 'တေ', '▁လမ်း', 'စဉ်', 'သည်', '▁ရခိုင်ပြည်', '▁', 'လွတ်လပ်ရေး', '▁', 'အတွက်', 'ဦး', 'တည်', 'သည်။']
 k = 3
 prev = a[0:k]
