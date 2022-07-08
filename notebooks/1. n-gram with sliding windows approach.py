94360d3b20c07418ec561a6bc4c207c6668508b9
Sa Phyo Thu Htet <phyothuhtet.ai@gmail.com>
2022-07-08 13:45:13 +0630

    Create 1. n-gram with sliding windows approach.py

---
+++ notebooks/1. n-gram with sliding windows approach.py
@@ -0,0 +1,10 @@
+a = ['▁', 'al', 'p', '▁ဗိုလ်', 'ချူပ်', 'တေ', '▁လမ်း', 'စဉ်', 'သည်', '▁ရခိုင်ပြည်', '▁', 'လွတ်လပ်ရေး', '▁', 'အတွက်', 'ဦး', 'တည်', 'သည်။']
+k = 3
+prev = a[0:k]
+#print(''.join([str(elem) for elem in prev]))
+
+for i in range(k-1,len(a)):
+  print(prev)
+  print(''.join([str(elem) for elem in prev]))
+  if (i+1 < len(a)):
+      prev = prev[1:]+[a[i+1]]
