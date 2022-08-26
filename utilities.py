48d0b73d07e20d101f4c3d8ae40eee0eb0659ba0
Sa Phyo Thu Htet <phyothuhtet.ai@gmail.com>
2022-08-26 10:06:05 +0630

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -2,6 +2,6 @@
 import re
 
 def multilingual_semi_syllable_break(user_input):
-  result = re.sub(r"([a-zA-Z]+|[අ-෉][්-෥ෲ-ෳ඀-඄ි]{0,}|[அ-஽][஀-஄ா-௏ௗ]{0,}|[က-ဪဿ၌-၏ၐ-ၕၚ-ၝၡၥၦၮ-ၰၵ-ႁႎ႐-႙႟][ါ-ှၖ-ၙၞ-ၠၢ-ၤၧ-ၭၱ-ၴႂ-ႍႏႚ-႞]{0,}|[^ ])",r"\1 ", user_input)
+  result = re.sub(r"([a-zA-Z]+|[අ-෉][්-෥ෲ-ෳ඀-඄ි]{0,}|[அ-஽][஀-஄ா-௏ௗ]{0,}|[က-ဪဿ၌-၏ၐ-ၕၚ-ၝၡၥၦၮ-ၰၵ-ႁႎ႐-႙႟][ါ-ှၖ-ၙၞ-ၠၢ-ၤၧ-ၭၱ-ၴႂ-ႍႏႚ-႞]{0,}|[^ ])",r"\1--------------", user_input)
   result = re.sub(r" +", " ", result)
   return result
