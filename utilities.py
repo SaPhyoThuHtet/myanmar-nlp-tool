d34d9602eb6865e987b18eaff20a24827dee0b88
Sa Phyo Thu Htet <phyothuhtet.ai@gmail.com>
2022-08-26 09:49:02 +0630

    Create utilities.py

---
+++ utilities.py
@@ -0,0 +1,6 @@
+# Multilingual Semi Syllabe Break (Sinhala, Tamil, Myanmar (Burmese), Shan, Rakhine, Mon, Paoh)
+
+def multilingual_semi_syllable_break(user_input):
+  result = re.sub(r"([a-zA-Z]+|[අ-෉][්-෥ෲ-ෳ඀-඄ි]{0,}|[அ-஽][஀-஄ா-௏ௗ]{0,}|[က-ဪဿ၌-၏ၐ-ၕၚ-ၝၡၥၦၮ-ၰၵ-ႁႎ႐-႙႟][ါ-ှၖ-ၙၞ-ၠၢ-ၤၧ-ၭၱ-ၴႂ-ႍႏႚ-႞]{0,}|[^ ])",r"\1 ", user_input)
+  result = re.sub(r" +", " ", result)
+  return result
