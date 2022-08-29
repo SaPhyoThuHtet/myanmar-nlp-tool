acb66dfa4ef2ec667a4ef5466c63f894f8802b04
Sa Phyo Thu Htet <phyothuhtet.ai@gmail.com>
2022-08-29 23:54:53 +0630

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -2,7 +2,7 @@ import re
 import streamlit as st
 
 def multilingual_semi_syllable_break(user_input):
-  # Multilingual Semi Syllabe Break (Sinhala, Tamil, Myanmar (Burmese), Shan, Rakhine, Mon, Paoh)
+  # Multilingual Semi Syllabe Break (Sinhala, Tamil, Shan, Mon, Pali and Sanskrit, Sagaw Karen, Western Poh Karen, Eastern Poh Karen, Geba Karen, Kayah, Rumai Palaung, Khamathi Shan, Aiton and Phake, Burmese (Myanmar), Paoh, Rakhine Languages)
   result = re.sub(r"([a-zA-Z]+|[අ-෉][්-෥ෲ-ෳ඀-඄ි]{0,}|[அ-஽][஀-஄ா-௏ௗ]{0,}|[က-ဪဿ၌-၏ၐ-ၕၚ-ၝၡၥၦၮ-ၰၵ-ႁႎ႐-႙႟][ါ-ှၖ-ၙၞ-ၠၢ-ၤၧ-ၭၱ-ၴႂ-ႍႏႚ-႞]{0,}|.)",r"\1--------------", user_input)
   result = re.sub(r" +", " ", result)
   return result
