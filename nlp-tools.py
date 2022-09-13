7f8d29de0ea82ba206a66224dff9cdcef7bdcc97
Sa Phyo Thu Htet <phyothuhtet.ai@gmail.com>
2022-09-13 11:02:50 +0630

    Update nlp-tools.py

--- nlp-tools.py
+++ nlp-tools.py
@@ -47,8 +47,8 @@ if (option == 'syllable-tokenization-zawgyi'):
     user_input = st.text_input("Input", "သီဟိုဠ္မွ ဉာဏ္ႀကီးရွင္သည္ အာယုဝၯနေဆးၫႊန္းစာကို ဇလြန္ေဈးေဘး ဗာဒံပင္ထက္ အဓိ႒ာန္လ်က္ ဂဃနဏဖတ္ခဲ့သည္။ေဆာင္")
     result = re.sub(r'(ေ*ျ*ႀ*ၿ*ၾ*[က-အၫ|႐|ႏ|ဥ|ဦ|႒]([က-အ]့*္[့း]*|[ါ-ာ]|[ိ-ူ]|[ဲ-္]|်|[ြ-ှ]|[ၐ-ၽ]|[ႁ-ႎ]|[႑-႟]){0,}|.)',r'\1 ',user_input)
     new_rule = re.sub(r'([ေျႀၿၾ]*[က-ဪႏဿႎၐ-ၕၛ-ၝၡၦၮ-ၰၵ-ၽႁ႞-႟][ါ-ှ|ၖ-ၚ|ၞ-ၠ|ၢ-ၤ|ၧ-ၭ|ၱ-ၴ|ႂ-ႍ|ႚ-ႝ]{0,}|.)', r'\1 ', user_input)
-    #st.write("Output:",result)
-    st.write("Output:",new_rule)
+    st.write("Output:",result)
+    #st.write("Output:",new_rule)
     
 if (option == 'multilingual_semi_syllable_tokenization'):
     
