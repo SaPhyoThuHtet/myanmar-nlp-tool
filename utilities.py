e494785fdf6d8f31dbf3ba86169796f3358bd849
Sa Phyo Thu Htet <phyothuhtet.ai@gmail.com>
2023-01-09 22:44:19 +0630

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -24,7 +24,7 @@ def zawgyi_unicode_detection(input:str)->str:
     zawgyi_unicode_tokenizer = load_zawgyi_unicode_tokenizer()
     model = load_zawgyi_unicode_detection_model()
     st.write("Syllbreak:", syllable_tokenization(input))
-    testing_sequences = zawgyi_unicode_tokenizer.texts_to_sequences([syllable_tokenization(input)])
+    testing_sequences = zawgyi_unicode_tokenizer.texts_to_sequences(syllable_tokenization(input))
     testing_padded = pad_sequences(testing_sequences,maxlen=150, truncating='post',padding='post')
     st.write(testing_padded)
     ans = model.predict(testing_padded)
