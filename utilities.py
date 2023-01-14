e2a90252aad4868a137eb9e414f36e29aedb96e4
Sa Phyo Thu Htet <phyothuhtet.ai@gmail.com>
2023-01-14 21:05:28 +0630

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -46,7 +46,7 @@ def keywords_detection(lexicon:str, input:str):
     keywords = ""
     for i in lexicon.strip().split("|||"):
         keywords +=i.lower().replace("$","\$").replace(" ", "")+"(?![ါ-ှ]|[က-အ]်)"+"|"
-    
+    keywords = keywords[0:-1]
     return re.findall(f"{keywords}",input)
 
 """
